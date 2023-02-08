818cf13b680b6c3ff5695981ca1400bcf80a5393
Fithamlak <justiceofgod01@gmail.com>
2023-02-08 22:44:23 +0300

    Implement Function To Display all Scores During Refresh is clicked

--- src/modules.js/refresh.js
+++ src/modules.js/refresh.js
@@ -2,8 +2,8 @@ import { feachScores } from './game.js';
 import display from './desplay.js';
 
 export const refresh = async () => {
-  const tbody = document.getElementsByTagName('tbody')[0];
-  tbody.innerHTML = '';
+  const table = document.querySelector('#tb');
+  table.innerHTML = '';
   const games = await feachScores();
   games.forEach((game) => {
     display(game);
